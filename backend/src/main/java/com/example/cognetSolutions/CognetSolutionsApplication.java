3972ad7ea1a64540c2538f49b99c018b4115ff15
Ishuwara Avarjana <ishuwarawedage@gmail.com>
2025-05-03 13:35:32 +0530

    Backend initialize

---
+++ backend/src/main/java/com/example/cognetSolutions/CognetSolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.cognetSolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CognetSolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CognetSolutionsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/cognetSolutions/CognetSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cognetSolutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CognetSolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
