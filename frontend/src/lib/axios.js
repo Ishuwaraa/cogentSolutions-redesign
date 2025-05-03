85391ce4ddf0df4955f0176ae5aa4ab434a7cb8d
Ishuwara Avarjana <ishuwarawedage@gmail.com>
2025-05-03 22:29:26 +0530

    API url change

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8080/api'
+    baseURL: 'https://cogentsolutions-redesign-production.up.railway.app/api'
 });
