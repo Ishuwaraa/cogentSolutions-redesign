89c086bafc0b5422b97a00a6b0105e2b3a3301ad
Ishuwara Avarjana <ishuwarawedage@gmail.com>
2025-05-04 10:13:26 +0530

    Timestamp add for user model

--- backend/src/main/java/com/example/cognetSolutions/model/User.java
+++ backend/src/main/java/com/example/cognetSolutions/model/User.java
@@ -4,8 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -20,6 +24,12 @@ public class User {
     private String lastName;
     private String email;
     private String message;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 
     public User(String firstName, String lastName, String email, String message) {
         this.firstName = firstName;
